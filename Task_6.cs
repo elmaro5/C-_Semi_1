83cf0f53d728ae9dd00f112b0092a23003cfe09f
fiji <kremus13@gmail.com>
2023-04-30 01:14:10 +0300

    Task_6

---
+++ Task_6.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
